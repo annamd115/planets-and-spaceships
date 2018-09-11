afc494630db0ee85e8952680cb3ae3a4f7d095aa
annamd115 <anna.dolan115@gmail.com>
2018-09-10 19:21:59 -0500

    added probes

--- PlanetAndSpaceships/Program.cs
+++ PlanetAndSpaceships/Program.cs
@@ -36,10 +36,41 @@ namespace PlanetAndSpaceships
             //Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the Remove() method to eliminate it from the end of planetList.
             combinedPlanetList.Remove("Pluto");
 
+            var probes = new Dictionary<string, List<string>>
+            {
+                { "Mariner 10", new List<string> { "Mercury", "Earth"} },
+                { "Messenger", new List<string> { "Mercury", "Venus" } },
+                { "BepiColombo",  new List<string> { "Mercury", } },
+                { "Venus Express", new List<string>{ "Venus",} },
+                { "Parker Solar Probe", new List<string>{ "Venus",} },
+                { "Pioneer 10", new List<string> { "Earth", "Jupiter"} },
+                { "Pioneer 11", new List<string> { "Earth", "Saturn", "Jupiter"} },
+                { "Voyager 1", new List<string>{ "Earth", "Jupiter", "Saturn"} },
+                { "Voyager 2", new List<string>{ "Earth", "Jupiter", "Saturn", "Uranus", "Neptune"} },
+                { "Mariner 3", new List<string>{ "Mars"} },
+                { "Mariner 4", new List<string>{ "Mars"} },
+                { "Mariner 6", new List<string>{ "Mars"} },
+                { "Mariner 7", new List<string>{ "Mars"} },
+                { "New Horizons", new List<string>{ "Jupiter", "Pluto"} }
+            };
+
             foreach (string planet in combinedPlanetList)
             {
-                Console.WriteLine(planet);
+                var visitedProbe = new List<string>();
+
+                foreach (var probe in probes)
+                {
+                    if (probe.Value.Contains(planet))
+                    {
+                        visitedProbe.Add(probe.Key);
+
+                    }
+                }
+                string visitedProbes = string.Join(", ", visitedProbe);
+                Console.WriteLine($"{planet}: {visitedProbes}");
             }
+
+
 
             Console.ReadLine();
         }
